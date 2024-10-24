69167cc5837b2f05458039717fa0d38dbb4757d5
Gabriel Foganholi <gabrielfoganholi@hotmail.com>
2024-10-24 19:09:37 -0300

    bootstrap no css

--- script.js
+++ script.js
@@ -9,35 +9,37 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
+// Alternar o menu responsivo usando o Bootstrap
+const navbarToggler = document.querySelector('.navbar-toggler');
+const navMenu = document.querySelector('.navbar-collapse');
 
-// Alternar o menu responsivo
-const menuToggle = document.querySelector('#mobile-menu');
-const navList = document.querySelector('.nav-list');
-
-menuToggle.addEventListener('click', function() {
-    navList.classList.toggle('active');
-
-    // Impede a rolagem do corpo quando o menu está aberto
-    if (navList.classList.contains('active')) {
-        document.body.style.overflow = 'hidden'; // Desabilita a rolagem do corpo
-    } else {
+navbarToggler.addEventListener('click', function() {
+    if (navMenu.classList.contains('show')) {
         document.body.style.overflow = ''; // Restaura a rolagem do corpo
+    } else {
+        document.body.style.overflow = 'hidden'; // Desativa a rolagem quando o menu está aberto
     }
+});
+
+// Fechar o menu responsivo ao clicar em um item de navegação (opcional)
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function() {
+        if (navMenu.classList.contains('show')) {
+            navbarToggler.click(); // Fecha o menu ao clicar em um item
+        }
+    });
 });
 
 // Prevenir rolagem horizontal
 window.addEventListener('touchmove', function(e) {
-    if (navList.classList.contains('active')) {
-        e.preventDefault(); // Impede a rolagem horizontal apenas se o menu estiver ativo
+    if (navMenu.classList.contains('show')) {
+        e.preventDefault(); // Impede a rolagem horizontal quando o menu está aberto
     }
 }, { passive: false });
 
-
-// Impede a rolagem do corpo se o menu estiver ativo
+// Impede a rolagem do corpo se o menu estiver ativo (para rolagem via mouse)
 document.addEventListener('wheel', function(event) {
-    if (navList.classList.contains('active')) {
+    if (navMenu.classList.contains('show')) {
         event.preventDefault(); // Impede a rolagem do corpo
     }
 }, { passive: false });
-
-
